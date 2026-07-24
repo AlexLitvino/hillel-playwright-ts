0667ada595f69fc42b7b087c562ae6b840ad4c27
AlexLitvino <litvinov.aleks@gmail.com>
2026-07-24 13:08:42 +0300

    lesson29 reporters

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,6 +26,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  // reporter: "allure-playwright",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -41,6 +42,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    video: 'retain-on-failure',
+    screenshot: 'on-first-failure',
 
     // launchOptions: {
     //   slowMo: 1000,
